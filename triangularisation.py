9db08b38d545595a83bc9b5a947e9d793db3fd4f
Guillaume <37037759+guillaume247@users.noreply.github.com>
2018-03-04 11:38:40 +0100

    ajout de mesure_position

--- triangularisation.py
+++ triangularisation.py
@@ -11,7 +11,7 @@
 from __future__ import print_function
 import time
 from math import *
-
+from hc_sr04_simul import *
 
 def measure_average(capteur):
   """
@@ -28,11 +28,11 @@ def measure_average(capteur):
   float : distance en cm
   """
 
-  distance1=measure(capteur)
+  distance1=mesure(capteur)
   time.sleep(0.1)
-  distance2=measure(capteur)
+  distance2=mesure(capteur)
   time.sleep(0.1)
-  distance3=measure(capteur)
+  distance3=mesure(capteur)
   distance = distance1 + distance2 + distance3
   distance = distance / 3
   return distance
@@ -63,11 +63,28 @@ def calcule_position(c1, c2, d1, d2):
     p2[X]: coordonnée X du point 2 (en cm)
     p2[Y]: coordonnée Y du point 2 (en cm)
   """
+  
+  N=(d2**2-d1**2-c2[X]**2+c1[X]**2-c2[Y]**2+c1[Y]**2)/2*(c1[Y]-c2[Y])
+  A=(c1[X]-c2[X])/(c1[Y]-c2[Y])**2+1
+  B=2*c1[Y]*(c1[X]-c2[X])/(c1[Y]-c2[Y])-2*N*(c1[X]-c2[X])/(c1[Y]-c2[Y])-2*c1[X]
+  C=c1[X]**2+c1[Y]**2+N**2-d1**2-2*c1[Y]*N
+  Disc=B**2-4*A*C
 
-  # Pour calculer une racine carrée, utiliser sqrt(). Exemple: sqrt(2)
+  if Disc>0:
+    p1={}
+    p2={}
+    p1[X]=(-B+sqrt(Disc))/2*A
+    p2[X]=(-B-sqrt(Disc))/2*A
+    p1[Y]=N-p1[X]*(c1[X]-c2[X])/(c1[Y]-c2[Y])
+    p2[Y]=N-p2[X]*(c1[X]-c2[X])/(c1[Y]-c2[Y])
+    return (p1, p2)
 
-  return (p1, p2)
+  if Disc==0:
+    p1[X]=-B/2*A
+    p1[Y]=N-p1[X]*(c1[X]-c2[X]/c1[Y]-c2[Y])
 
+  if Disc<0:
+    return None
 
 # -----------------------
 # Main Script
@@ -75,7 +92,7 @@ def calcule_position(c1, c2, d1, d2):
 
 # Use BCM GPIO references
 # instead of physical pin numbers
-GPIO.setmode(GPIO.BCM)
+#GPIO.setmode(GPIO.BCM)
 
 # Clefs pour définir les caractéristiques d'un capteur de distance
 TRIG = "TRIG"   # Pin pour déclencher la mesure de distance
@@ -90,16 +107,16 @@ speedSound = 33100 + (0.6*temperature)
 print("Ultrasonic Measurement")
 print("Speed of sound is", speedSound/100, "m/s at ", temperature, "deg")
 
-capteurs = [ {TRIG: 23, ECHO: 24}, {TRIG: 5, ECHO: 6} ]
+capteurs = [ {TRIG: 23, ECHO: 24, X: 0, Y: 0}, {TRIG: 5, ECHO: 6, X: 100, Y: 100} ]
 
 # Configure les pins: TRIG -> output, ECHO -> input
-for capteur in capteurs:
-  GPIO.setup(capteur[TRIG], GPIO.OUT)
-  GPIO.setup(capteur[ECHO], GPIO.IN)
+#for capteur in capteurs:
+#  GPIO.setup(capteur[TRIG], GPIO.OUT)
+#  GPIO.setup(capteur[ECHO], GPIO.IN)
 
 # Set trigger to False (Low)
-for capteur in capteurs:
-  GPIO.output(capteur[TRIG], False)
+#for capteur in capteurs:
+#  GPIO.output(capteur[TRIG], False)
 
 # Allow module to settle
 time.sleep(0.5)
@@ -110,12 +127,17 @@ time.sleep(0.5)
 # GPIO cleanup function. This will also prevent
 # the user seeing lots of unnecessary error
 # messages.
+d=range(2)
 try:
   while True:
     for i in range(len(capteurs)):
-      distance = measure_average(capteurs[i])
-      print("Distance {0:1d}: {1:5.1f}".format(i, distance))
-      time.sleep(1)
+      d[i] = measure_average(capteurs[i])
+      print("Distance {0:1d}: {1:5.1f}".format(i, d[i]))
+
+    P=calcule_position(capteurs[0], capteurs[1], d[0], d[1])
+    print(P)
+
+    time.sleep(1)
     print("")
 
 except KeyboardInterrupt:
